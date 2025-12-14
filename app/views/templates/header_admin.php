ab3bf1af7d619f52cf74719d37fe76698440f151
PraNAtaP <pranatapu08@gmail.com>
2025-12-14 12:51:45 +0700

    add About us

--- app/views/templates/header_admin.php
+++ app/views/templates/header_admin.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data['judul']; ?></title>
-    <link rel="icon" type="image/png" href="<?= BASEURL; ?>/public/img/LOGO-icon.png" />
-    <link rel="shortcut icon" href="<?= BASEURL; ?>/public/img/LOGO-icon.png" />
+    <link rel="icon" type="image/png" href="<?= BASEURL; ?>/img/LOGO-icon.png" />
+    <link rel="shortcut icon" href="<?= BASEURL; ?>/img/LOGO-icon.png" />
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/admin.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
--- app/views/templates/header_public.php
+++ app/views/templates/header_public.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" type="image/png" href="<?= BASEURL; ?>/public/img/LOGO-icon.png" />
-  <link rel="shortcut icon" href="<?= BASEURL; ?>/public/img/LOGO-icon.png" />
+  <link rel="icon" type="image/png" href="<?= BASEURL; ?>/img/LOGO-icon.png" />
+  <link rel="shortcut icon" href="<?= BASEURL; ?>/img/LOGO-icon.png" />
   <title><?= $data['judul']; ?></title>
   <link
     rel="stylesheet"

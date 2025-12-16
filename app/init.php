ec137dab5c360a3f7dc1ca944c19ae3b8737f9d0
PraNAtaP <pranatapu08@gmail.com>
2025-12-16 13:58:39 +0700

    Update Keri njir

--- app/init.php
+++ app/init.php
@@ -1,10 +1,9 @@
 <?php
-if( !session_id() ) session_start();
+if (!session_id()) session_start();
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/core/App.php';
 require_once __DIR__ . '/core/Controller.php';
 require_once __DIR__ . '/core/Database.php';
-require_once __DIR__ . '/core/Flasher.php'; 
 spl_autoload_register(function ($class) {
     $path = __DIR__ . '/core/' . $class . '.php';
     if (file_exists($path)) require_once $path;
